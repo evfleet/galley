92193c1daa1e5673d310d97ce3bf62d4b7fe3002
Evan Fleet <evfleet@gmail.com>
2024-01-05 14:54:11 -0500

    feat(api): setup logging

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -1,9 +1,14 @@
 import express from "express";
+import pino from "pino-http";
+
+import { logger } from "@/config/logger";
 
 export async function build() {
   const app = express();
 
   app.use(express.json());
+
+  app.use(pino({ logger }));
 
   return app;
 }
