2702d93b7ca9881f543c4e2b03b1af63e5768b66
Evan Fleet <evfleet@gmail.com>
2024-01-05 09:33:12 -0500

    fix(web): set devtools to only production (#10)

--- packages/web/src/routes/index.tsx
+++ packages/web/src/routes/index.tsx
@@ -1,5 +1,15 @@
+import React from "react";
 import { Outlet, RootRoute, Route, Router } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+
+// eslint-disable-next-line
+const TanStackRouterDevtools =
+  process.env.NODE_ENV === "production"
+    ? () => null
+    : React.lazy(() =>
+        import("@tanstack/router-devtools").then((res) => ({
+          default: res.TanStackRouterDevtools,
+        }))
+      );
 
 const rootRoute = new RootRoute({
   component: () => (
