4cd2192c7a657f4da76aef09654c30be6866f858
Evan Fleet <evfleet@gmail.com>
2024-01-05 09:02:33 -0500

    build(web): setup react vite app

---
+++ packages/web/src/App.tsx
@@ -0,0 +1,14 @@
+import { message } from "@galley/common";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Hello World</h1>
+        <p>{message}</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
