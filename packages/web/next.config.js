ca8d21bbca1d03a3cfd276deb9fc920a911da343
Evan Fleet <evfleet@gmail.com>
2024-01-07 13:08:44 -0500

    build(web): test fix with vercel

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@galley/common"],
+  webpack: (config, _context) => {
+    config.resolve.alias["@galley/common"] = path.resolve(
+      __dirname,
+      "node_modules/@galley/common"
+    );
+    return config;
+  },
 };
 
 module.exports = nextConfig;
