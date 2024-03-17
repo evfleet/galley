7ecabe17b60d58042efc06d378504809320d0884
Evan Fleet <evfleet@gmail.com>
2024-03-17 14:35:29 -0400

    feat(web): add ingredients and improve ux

--- packages/web/src/features/recipes/components/RecipeForm.tsx
+++ packages/web/src/features/recipes/components/RecipeForm.tsx
@@ -1,13 +1,27 @@
 import { useForm, useFieldArray } from "react-hook-form";
 
 export function RecipeForm() {
-  const { control, register, handleSubmit } = useForm({
+  const { control, register, handleSubmit, watch } = useForm({
     defaultValues: {
+      ingredients: [{ ingredient: "" }],
       directions: [{ direction: "" }],
     },
   });
 
-  const { fields, append } = useFieldArray({
+  const {
+    fields: ingredientFields,
+    append: ingredientAppend,
+    remove: ingredientRemove,
+  } = useFieldArray({
+    control,
+    name: "ingredients",
+  });
+
+  const {
+    fields: directionFields,
+    append: directionAppend,
+    remove: directionRemove,
+  } = useFieldArray({
     control,
     name: "directions",
   });
@@ -20,14 +34,54 @@ export function RecipeForm() {
     <div>
       <h1>Recipe Form</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
-        {fields.map((field, index) => (
-          <div key={field.id}>
-            <input {...register(`directions.${index}.direction`)} />
-            <button type="button" onClick={() => append({ direction: "" })}>
-              Add
-            </button>
-          </div>
-        ))}
+        <div>
+          <h2>Ingredients</h2>
+          {ingredientFields.map((field, index) => (
+            <div key={field.id}>
+              <input {...register(`ingredients.${index}.ingredient`)} />
+              {ingredientFields.length > 1 && (
+                <button type="button" onClick={() => ingredientRemove(index)}>
+                  Delete
+                </button>
+              )}
+            </div>
+          ))}
+
+          <button
+            type="button"
+            disabled={
+              watch("ingredients")[ingredientFields.length - 1].ingredient ===
+              ""
+            }
+            onClick={() => ingredientAppend({ ingredient: "" })}
+          >
+            Add
+          </button>
+        </div>
+
+        <div>
+          <h2>Directions</h2>
+          {directionFields.map((field, index) => (
+            <div key={field.id}>
+              <input {...register(`directions.${index}.direction`)} />
+              {directionFields.length > 1 && (
+                <button type="button" onClick={() => directionRemove(index)}>
+                  Delete
+                </button>
+              )}
+            </div>
+          ))}
+
+          <button
+            type="button"
+            disabled={
+              watch("directions")[directionFields.length - 1].direction === ""
+            }
+            onClick={() => directionAppend({ direction: "" })}
+          >
+            Add
+          </button>
+        </div>
 
         <button type="submit">Submit</button>
       </form>
