fb937bedfd72046fa06c37ca7d00996d70423eca
Evan Fleet <evfleet@gmail.com>
2024-03-19 15:56:45 -0400

    build(web): setup api mocking

--- packages/web/src/main.tsx
+++ packages/web/src/main.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createServer } from "miragejs";
 
 import "./main.css";
 import App from "./App.tsx";
+
+createServer({
+  routes() {
+    this.namespace = "api";
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
