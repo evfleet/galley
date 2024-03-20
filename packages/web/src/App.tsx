29610f87eda46d9b068d0639fd8534d0664ab490
Evan Fleet <evfleet@gmail.com>
2024-03-19 20:07:36 -0400

    build(web): setup mocks to not import in prod

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -2,8 +2,6 @@ import { RouterProvider } from "react-router-dom";
 
 import { router } from "@/routes";
 
-function App() {
+export function App() {
   return <RouterProvider router={router} />;
 }
-
-export default App;
