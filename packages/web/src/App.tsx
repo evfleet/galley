66ae7e568564d37008d4721d7b243718f559921d
Evan Fleet <evfleet@gmail.com>
2024-03-20 14:11:20 -0400

    feat(web): setup query client

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,7 +1,16 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { RouterProvider } from "react-router-dom";
 
 import { router } from "@/routes";
 
+const queryClient = new QueryClient();
+
 export function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
 }
