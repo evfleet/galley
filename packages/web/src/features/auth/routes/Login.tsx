96d02b5ebdb6e28617c503877138fd5ad64726fc
Evan Fleet <evfleet@gmail.com>
2024-03-23 15:02:25 -0400

    feat(web): basic login form

--- packages/web/src/features/auth/routes/Login.tsx
+++ packages/web/src/features/auth/routes/Login.tsx
@@ -1,7 +1,30 @@
 export function LoginPage() {
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="flex flex-col min-w-screen min-h-screen gap-4 p-8">
+      <h1 className="text-4xl font-bold text-center">Login</h1>
+
+      <form className="flex flex-col gap-4 flex-1 justify-end">
+        <input className="border-2 p-2" type="text" placeholder="Username" />
+        <input
+          className="border-2 p-2"
+          type="password"
+          placeholder="Password"
+        />
+        <button className="border-2 p-2 bg-blue-400">Login</button>
+      </form>
+
+      <div className="flex flex-col gap-2 items-center">
+        <p>Or sign in with</p>
+
+        <div className="flex flex-col gap-2">
+          <button className="border-2 p-2">Sign in with Google</button>
+          <button className="border-2 p-2">Sign in with Facebook</button>
+        </div>
+      </div>
+
+      <span className="text-center">
+        Don't have an account? <a href="#">Sign up</a>
+      </span>
     </div>
   );
 }
