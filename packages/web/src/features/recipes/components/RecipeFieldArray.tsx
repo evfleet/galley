28f6f11c80ac09761fca5a53f03fd755fb4fde03
Evan Fleet <evfleet@gmail.com>
2024-03-29 16:56:40 -0400

    test(web): expand tests for recipe form

--- packages/web/src/features/recipes/components/RecipeFieldArray.tsx
+++ packages/web/src/features/recipes/components/RecipeFieldArray.tsx
@@ -43,25 +43,34 @@ export function RecipeFieldArray({
       </legend>
 
       <ul>
-        {fields.map((field, index) => (
-          <li key={field.id} className="flex">
-            <input
-              {...register(`${name}.${index}.value`)}
-              aria-label={`${name} ${index + 1}`}
-              className="w-11/12 border-2"
-            />
-            {fields.length > 1 && (
-              <button
-                type="button"
-                onClick={() => remove(index)}
-                className="flex w-1/12 items-center justify-center"
-              >
-                <Trash2 />
-                <span className="sr-only">{`Delete ${name} ${index + 1}`}</span>
-              </button>
-            )}
-          </li>
-        ))}
+        {fields.map((field, index) => {
+          const labelText = `${name}-${index + 1}`;
+
+          return (
+            <li key={field.id} className="flex">
+              <label htmlFor={labelText} className="sr-only">
+                {labelText}
+              </label>
+              <input
+                {...register(`${name}.${index}.value`)}
+                id={labelText}
+                className="w-11/12 border-2"
+              />
+              {fields.length > 1 && (
+                <button
+                  type="button"
+                  onClick={() => remove(index)}
+                  className="flex w-1/12 items-center justify-center capitalize"
+                >
+                  <Trash2 />
+                  <span className="sr-only">{`Delete ${name.slice(0, -1)} ${
+                    index + 1
+                  }`}</span>
+                </button>
+              )}
+            </li>
+          );
+        })}
       </ul>
 
       <div className="flex justify-end">
@@ -69,9 +78,9 @@ export function RecipeFieldArray({
           type="button"
           disabled={watch(name)[fields.length - 1].value === ""}
           onClick={() => append({ value: "" })}
-          className="bg-gray-400 px-2 py-1"
+          className="bg-gray-400 px-2 py-1 capitalize"
         >
-          Add
+          Add {name.slice(0, -1)}
         </button>
       </div>
     </fieldset>
