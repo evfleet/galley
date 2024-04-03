b037e72a05d5876f5194f8a6d4a295c30a145462
Evan Fleet <evfleet@gmail.com>
2024-04-03 19:03:40 -0400

    build(api): fix typos in schema and setup util to create tables

---
+++ packages/api/src/database/utils/create-tables.ts
@@ -0,0 +1,17 @@
+import fs from "node:fs";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+import { db } from "../database.js";
+
+export function createTables() {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  const file = path.resolve(__dirname, "../schema.sql");
+  const sql = fs.readFileSync(file, "utf-8");
+
+  db.exec(sql);
+}
+
+createTables();
