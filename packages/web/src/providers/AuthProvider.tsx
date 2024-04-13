e540213455cceb9f73a7f1336db08b8ea5df3761
Evan Fleet <evfleet@gmail.com>
2024-04-13 16:17:41 -0400

    feat: setup structure for auth check

--- packages/web/src/providers/AuthProvider.tsx
+++ packages/web/src/providers/AuthProvider.tsx
@@ -1,18 +1,34 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 import { Outlet } from "react-router-dom";
 
 type AuthContextType = {
   user?: string;
   setUser: (user: string) => void;
+  status: string;
 };
+
+type AuthStatus = "initializing" | "ready";
 
 export const AuthContext = createContext<AuthContextType | null>(null);
 
 export function AuthProvider() {
   const [user, setUser] = useState("test");
+  const [status, setStatus] = useState<AuthStatus>("initializing");
+
+  useEffect(() => {
+    async function fetchUser() {
+      const res = await fetch("/api/v1/auth/");
+
+      console.log(res);
+
+      setStatus("ready");
+    }
+
+    fetchUser();
+  }, []);
 
   return (
-    <AuthContext.Provider value={{ user, setUser }}>
+    <AuthContext.Provider value={{ user, setUser, status }}>
       <Outlet />
     </AuthContext.Provider>
   );
