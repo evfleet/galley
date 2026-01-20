6bef117bde988908fb132d75dc42e18efda2c819
Evan Fleet <evfleet@gmail.com>
2026-01-20 14:57:52 -0500

    build(web): initial commit for web

---
+++ packages/web/src/routes/index.tsx
@@ -0,0 +1,11 @@
+import { createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/")({ component: App });
+
+function App() {
+	return (
+		<div>
+			<h1>Galley</h1>
+		</div>
+	);
+}
